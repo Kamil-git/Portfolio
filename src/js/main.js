e3c77d7c05a138c74d607172cb9744b7d5badadd
Kamil Kwiatkowski <kaczy.kwiatkowski@gmail.com>
2022-06-13 22:11:53 +0200

    color change js

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,40 @@
-$(document).ready(function () {
-  document.querySelector("a").addEventListener("click", () => {
-    smoothScroll(this.hash)
-  })
+const navbar = document.querySelectorAll(".header__nav > div > a")
+const projects = document.querySelector("#projects")
 
-  function smoothScroll(target) {
-    $("body,html").animate(
-      {
-        scrollTop: target.offset().top,
-      },
-      800
+// function checkVisible(elm) {
+//   let rect = elm.getBoundingClientRect()
+//   let viewHeight = Math.max(
+//     document.documentElement.clientHeight,
+//     window.innerHeight
+//   )
+//   return !(rect.bottom < 0 || rect.top - viewHeight >= 0)
+// }
+
+// checkVisible(projects)
+
+window.addEventListener('scroll', function(){
+  switch (
+    !(
+      projects.getBoundingClientRect().bottom < 0 ||
+      projects.getBoundingClientRect().top -
+        Math.max(document.documentElement.clientHeight, window.innerHeight) >=
+        0
     )
-  }
+  ) {
+    case true:
+      for(i=0; i < navbar.length; i++ ){
+        navbar[i].classList.add('active')
+        
+      }
+      console.log('true')
+      break
+    case false:
+      for (i = 0; i < navbar.length; i++) {
+        navbar[i].classList.remove("active")
+      }
+      console.log("false")
+      break
+    default:
+    
+  } 
 })
