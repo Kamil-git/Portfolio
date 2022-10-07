d0aa12037d7e8016d5e4b627e4ece5553636ce02
Kamil Kwiatkowski <kaczy.kwiatkowski@gmail.com>
2022-10-08 00:12:05 +0200

    karuzela

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,93 @@
-const navbar = document.querySelectorAll("header > div > a")
-const projects = document.querySelector("#projects")
+function scrollYHideNav() {
+  let lastScrollY = window.scrollY
+  let nav = document.querySelector(".nav")
+  window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+      nav.classList.add("nav--hidden")
+    } else {
+      nav.classList.remove("nav--hidden")
+    }
+    lastScrollY = window.scrollY
+  })
+}
 
-// function checkVisible(elm) {
-//   let rect = elm.getBoundingClientRect()
-//   let viewHeight = Math.max(
-//     document.documentElement.clientHeight,
-//     window.innerHeight
-//   )
-//   return !(rect.bottom < 0 || rect.top - viewHeight >= 0)
-// }
+function scrollYChangeColor() {
+  const navbar = document.querySelectorAll("header > div > a")
+  const projects = document.querySelector("#projects")
+  window.addEventListener("scroll", function () {
+    switch (
+      !(
+        projects.getBoundingClientRect().bottom < 0 ||
+        projects.getBoundingClientRect().top -
+          Math.max(document.documentElement.clientHeight, window.innerHeight) >=
+          0
+      )
+    ) {
+      case true:
+        for (i = 0; i < navbar.length; i++) {
+          navbar[i].classList.add("--active")
+        }
 
-// checkVisible(projects)
+        break
+      case false:
+        for (i = 0; i < navbar.length; i++) {
+          navbar[i].classList.remove("--active")
+        }
 
-window.addEventListener("scroll", function () {
-  switch (
-    !(
-      projects.getBoundingClientRect().bottom < 0 ||
-      projects.getBoundingClientRect().top -
-        Math.max(document.documentElement.clientHeight, window.innerHeight) >=
-        0
-    )
-  ) {
-    case true:
-      for (i = 0; i < navbar.length; i++) {
-        navbar[i].classList.add("--active")
-      }
+        break
+      default:
+    }
+  })
+}
 
-      break
-    case false:
-      for (i = 0; i < navbar.length; i++) {
-        navbar[i].classList.remove("--active")
-      }
-
-      break
-    default:
-  }
-})
+function projectsCarousel() {
+  const buttons = document.querySelectorAll("[data-carousel-button]")
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1
+      const slides = button
+        .closest("[data-carousel]")
+        .querySelector("[data-slides]")
+      const activeSlide = slides.querySelector("[data-active]")
+      let newIndex = [...slides.children].indexOf(activeSlide) + offset
+      if (newIndex < 0) newIndex = slides.children.length - 1
+      if (newIndex >= slides.children.length) newIndex = 0
+      slides.children[newIndex].dataset.active = true
+      delete activeSlide.dataset.active
+    })
+  })
+}
+function carousel() {
+  let buttonPrev = document.querySelector(".projects__button--prev")
+  let buttonNext = document.querySelector(".projects__button--next")
+  let slideContainer = document.querySelector("#projects__container")
+  let slideItem1 =
+    document.querySelector("[data-active1]").getBoundingClientRect().x - 80
+  let slideItem2 =
+    document.querySelector("[data-active2]").getBoundingClientRect().x - 80
+  let slideItem3 =
+    document.querySelector("[data-active3]").getBoundingClientRect().x - 80
+  let slideItem4 =
+    document.querySelector("[data-active4]").getBoundingClientRect().x - 80
+  
+  return buttonNext.addEventListener("click", () => {
+    switch (slideContainer.scrollLeft) {
+      case 0:
+        slideContainer.scrollTo(slideItem2, 0)
+        break
+      case slideItem2:
+        slideContainer.scrollTo(slideItem3, 0)
+        break
+      case slideItem3:
+        slideContainer.scrollTo(slideItem4, 0)
+        break
+      case slideItem4:
+        slideContainer.scrollTo(slideItem1, 0)
+        break
+    }
+  })
+}
+carousel()
+projectsCarousel()
+scrollYHideNav()
+scrollYChangeColor()
