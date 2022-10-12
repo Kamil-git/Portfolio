3966b6f46eac23b0818963eaa3b2cf2c35c41a89
Kamil Kwiatkowski <kaczy.kwiatkowski@gmail.com>
2022-10-12 17:12:00 +0200

    slider

--- src/js/main.js
+++ src/js/main.js
@@ -70,27 +70,156 @@ function carousel() {
     }
   })
 }
-function carouselSlide() {
-  const buttonNext = document.querySelector("[data-carousel-button='next']")
-  const slideContainer = document.querySelector("#projects__container")
-  let activeSlide = slideContainer.querySelector("[data-active]")
-  const projectSection = document.querySelector("#projects")
-  const paddingContainer = window
-    .getComputedStyle(projectSection, null)
-    .getPropertyValue("padding-left")
-    .split("px")[0] 
-  setTimeout(() => {
-    buttonNext.addEventListener("click", () => {
-      activeSlide = activeSlide.nextElementSibling
-      activeSlide !== null
-        ? slideContainer.scrollTo(activeSlide.getBoundingClientRect().x - paddingContainer, 0)
-        : (activeSlide = slideContainer.children[0])
-      slideContainer.scrollTo(activeSlide.getBoundingClientRect().x - paddingContainer, 0)
-    })
-  }, 1000)
-}
+// function carouselSlide() {
+//   const buttonNext = document.querySelector("[data-carousel-button='next']")
+//   const slideContainer = document.querySelector("#projects__container")
+//   let activeSlide = slideContainer.querySelector("[data-active]")
+//   const projectSection = document.querySelector("#projects")
+//   const paddingContainer = window
+//     .getComputedStyle(projectSection, null)
+//     .getPropertyValue("padding-left")
+//     .split("px")[0] 
+//   setTimeout(() => {
+//     buttonNext.addEventListener("click", () => {
+//       activeSlide = activeSlide.nextElementSibling
+//       activeSlide !== null
+//         ? slideContainer.scrollTo(activeSlide.getBoundingClientRect().x - paddingContainer, 0)
+//         : (activeSlide = slideContainer.children[0])
+//       slideContainer.scrollTo(activeSlide.getBoundingClientRect().x - paddingContainer, 0)
+//     })
+//   }, 1000)
+// }
+class Slider {
+            constructor(elemSelector) {
+                this.sliderSelector = elemSelector;
+                this.currentSlide = 0; //aktualny slide
+                this.time = null; //tutaj będziemy podczepiać setTimeout
+                this.slider = null;
+                this.elem = null;
+                this.slides = null;
+                this.prev = null; //przycisk prev
+                this.next = null; //przycisk next
+                this.dots = [];
 
+                this.generateSlider();
+                this.changeSlide(this.currentSlide);
+            }
+
+            generateSlider() {
+                //pobieramy element który zamienimy na slider-demo
+                this.slider = document.querySelector(this.sliderSelector);
+                this.slider.classList.add('slider');
+
+                //tworzymy kontener dla slajdow
+                const slidesCnt = document.createElement('div');
+                slidesCnt.classList.add('slider__slides');
+
+                //pobieramy element slajdów
+                this.slides = this.slider.children;
+
+                //to jest zywa kolekcja, więc przy przeniesieniu kazdego slajda
+                //jej dlugosc maleje
+                while (this.slides.length) {
+                    this.slides[0].classList.add('slider__slide');
+                    slidesCnt.appendChild(this.slides[0]);
+                }
+                this.slides = slidesCnt.querySelectorAll('.slider__slide');
+                this.slider.appendChild(slidesCnt);
+
+                this.createPrevNext();
+                this.createPagination();
+            }
+
+            slidePrev() {
+                this.currentSlide--;
+                if (this.currentSlide < 0) {
+                    this.currentSlide = this.slides.length - 1;
+                }
+                this.changeSlide(this.currentSlide);
+            }
+
+            slideNext() {
+                this.currentSlide++;
+                if (this.currentSlide > this.slides.length - 1) {
+                    this.currentSlide = 0;
+                }
+                this.changeSlide(this.currentSlide);
+            }
+
+            changeSlide(index) {
+                this.slides.forEach(slide => {
+                    slide.classList.remove('slider__slide--active');
+                    slide.setAttribute('aria-hidden', true);
+                });
+
+                //dodajemy ją tylko wybranemu
+                this.slides[index].classList.add('slider__slide--active');
+                this.slides[index].setAttribute('aria-hidden', false);
+
+                //podobny manewr robimy dla kropek
+                this.dots.forEach(dot => dot.classList.remove('slider__pagination__element--active'));
+                this.dots[index].classList.add('slider__pagination__element--active');
+
+                //aktualny slide przestawiamy na wybrany
+                this.currentSlide = index;
+            };
+
+            createPrevNext() {
+                this.prev = document.createElement('button');
+                this.prev.type = "button";
+                this.prev.innerText = "Poprzedni slide";
+                this.prev.classList.add('slider__nav__button');
+                this.prev.classList.add('slider__nav__button--prev');
+                this.prev.addEventListener('click', this.slidePrev.bind(this));
+
+                this.next = document.createElement('button');
+                this.next.type = "button";
+                this.next.innerText = "Następny slide";
+                this.next.classList.add('slider__nav__button');
+                this.next.classList.add('slider__nav__button--next');
+                this.next.addEventListener('click', this.slideNext.bind(this));
+
+                const nav = document.createElement('div');
+                nav.classList.add('slider__nav');
+                nav.appendChild(this.prev);
+                nav.appendChild(this.next);
+                this.slider.appendChild(nav);
+            };
+
+            createPagination() {
+                const ulDots = document.createElement('ul');
+                ulDots.classList.add('slider__pagination');
+
+                //tworzymy pętlę w ilości liczby slajdów
+                for (let i = 0; i < this.slides.length; i++) {
+                    //każdorazowo tworzymy LI wraz z buttonem
+                    //każdy button po kliknięciu zmieni slajd
+                    //za pomocą metody changeSlide()
+
+                    const li = document.createElement('li');
+                    li.classList.add('slider__pagination__element');
+
+                    const btn = document.createElement('button');
+                    btn.classList.add('slider__pagination__button');
+                    btn.type = "button";
+                    btn.innerText = i + 1;
+                    btn.setAttribute('aria-label', `Ustaw slajd ${i+1}`);
+
+                    btn.addEventListener('click', () => this.changeSlide(i));
+
+                    li.appendChild(btn);
+
+                    ulDots.appendChild(li);
+                    this.dots.push(li);
+                }
+
+                this.slider.appendChild(ulDots);
+            }
+        }
+
+
+const slide = new Slider('#slider1');
 // carousel()
-carouselSlide()
+
 scrollYHideNav()
 scrollYChangeColor()
